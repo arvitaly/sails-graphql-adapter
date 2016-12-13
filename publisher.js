1dca35194f97595698aa606c633aa4fab3b2a3d9
Alexandro <arvitaly@list.ru>
2016-12-13 20:18:48 +0700

    add globalid to publisher

--- publisher.js
+++ publisher.js
@@ -1,26 +1,30 @@
 "use strict";
 const graphql_models_1 = require("graphql-models");
 class SailsPublisher extends graphql_models_1.Publisher {
-    publishAdd(subscriptionId, modelId, added, context) {
-        context.request.socket.emit("subscription-" + subscriptionId, {
-            type: "add",
-            modelId,
+    publishAdd(subscriptionId, modelId, globalId, added, context) {
+        const message = {
+            kind: "add",
+            globalId,
+            id: subscriptionId,
             data: added,
-        });
+        };
+        context.request.socket.emit("live", message);
     }
-    publishRemove(subscriptionId, modelId, removed, context) {
+    publishRemove(subscriptionId, modelId, globalId, removed, context) {
         context.request.socket.emit("subscription-" + subscriptionId, {
             type: "remove",
             modelId,
             data: removed,
         });
     }
-    publishUpdate(subscriptionId, modelId, updated, context) {
-        context.request.socket.emit("subscription-" + subscriptionId, {
-            type: "update",
-            modelId,
+    publishUpdate(subscriptionId, modelId, globalId, updated, context) {
+        const message = {
             data: updated,
-        });
+            globalId,
+            id: subscriptionId,
+            kind: "update",
+        };
+        context.request.socket.emit("live", message);
     }
 }
 Object.defineProperty(exports, "__esModule", { value: true });
--- publisher.ts
+++ publisher.ts
@@ -1,29 +1,38 @@
 import { ModelID, Publisher, SubscriptionID } from "graphql-models";
+import { GlobalID, ILiveAddMessage, ILiveUpdateMessage } from "sails-graphql-interfaces";
 import Sails = require("sails");
 type Context = {
     request: Sails.Request;
 };
 class SailsPublisher extends Publisher {
-    public publishAdd(subscriptionId: SubscriptionID, modelId: ModelID, added, context: Context) {
-        context.request.socket.emit("subscription-" + subscriptionId, {
-            type: "add",
-            modelId,
+    public publishAdd(subscriptionId: SubscriptionID, modelId: ModelID, globalId: GlobalID, added, context: Context) {
+        const message: ILiveAddMessage = {
+            kind: "add",
+            globalId,
+            id: subscriptionId,
             data: added,
-        });
+        };
+        context.request.socket.emit("live", message);
     }
-    public publishRemove(subscriptionId: SubscriptionID, modelId: ModelID, removed, context: Context) {
+    public publishRemove(
+        subscriptionId: SubscriptionID, modelId: ModelID, globalId: GlobalID,
+        removed, context: Context) {
         context.request.socket.emit("subscription-" + subscriptionId, {
             type: "remove",
             modelId,
             data: removed,
         });
     }
-    public publishUpdate(subscriptionId: SubscriptionID, modelId: ModelID, updated, context: Context) {
-        context.request.socket.emit("subscription-" + subscriptionId, {
-            type: "update",
-            modelId,
+    public publishUpdate(
+        subscriptionId: SubscriptionID, modelId: ModelID, globalId: GlobalID,
+        updated, context: Context) {
+        const message: ILiveUpdateMessage = {
             data: updated,
-        });
+            globalId,
+            id: subscriptionId,
+            kind: "update",
+        };
+        context.request.socket.emit("live", message);
     }
 }
 export default SailsPublisher;
